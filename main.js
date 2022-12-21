9a0efd645a5249686df78f04a59bc4a21a8c0c26
kinghelios200 <103470405+kinghelios200@users.noreply.github.com>
2022-12-21 18:05:44 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function modelLoaded(){
+    console.log("Posenet Is Initialized!");
+}
+
+function draw(){
+    background("#228B22");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results)
+    }
+}
